3a770bf9045b6d0c97142c07ca44894f048e11b4
BossFreind1234 <vincentredtube@gmail.com>
2022-06-11 20:50:45 -0400

    fixed compile errors

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,8 +4,8 @@ import { themeChange } from "./helperFunctions/themeChange";  //Olspe: this is a
 
 const Navbar = () => {
     const [darkMode, setDarkMode] = useState(true);  //state to hold boolean value. true means darkMode is on
-    let themeIcon = <ion-icon name="moon-outline"></ion-icon>;  //default icon to show 
-    let sunnyIcon = <ion-icon name="sunny-outline"></ion-icon>;  //sunny icon 
+    let themeIcon = <ion-icon name="moon-outline"></ion-icon>;  //default icon to show
+    let sunnyIcon = <ion-icon name="sunny-outline"></ion-icon>;  //sunny icon
     let moonIcon = <ion-icon name="moon-sharp"></ion-icon>;  //moon icon
     //mode: match this variable' value to darkMode's value.
     //This gets passed into themeChange() since state varaibles should only be used in the same file it's made in
@@ -13,20 +13,20 @@ const Navbar = () => {
 
     function themeToggle() {  //this gets called when theme-btn gets clicked
         setDarkMode(!darkMode);  //change darkmode value
-        mode = darkMode;  
-        
+        mode = darkMode;
+
     }
 
     useEffect(function mount() {  //keeps track of changes to darkMode value and updates page styling each time
         themeChange(mode);
-        
+
     },[darkMode]);
 
     themeIcon = darkMode ? moonIcon : sunnyIcon;  //Olspe: this changes icon according to mode
 
-    return (        
+    return (
         <nav className="hamburger-menu">  {/* Olspe: Added classNames and html elements to make the burger icon */}
-
+            <div className="nav menu__box">
                 <a className="active ricebowl">🍚</a>
                 <a className="active">Certified_Rice</a>
                 <a href="../" className='menu__item'>Home</a>
@@ -38,11 +38,11 @@ const Navbar = () => {
                 <a href="https://github.com/CertifiedRice/certifiedrice-website" className='menu-item'><i className="fa-brands fa-github"></i>&nbsp; Source</a>
                 <button type="button" id="theme-btn" onClick={ themeToggle}>{themeIcon}</button>
             </div>
-            {/*Olspe: Added Script tag to access ionic-icons */}
-            <Script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></Script>
-            <Script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></Script>
-        </nav>
-    )
+    {/*Olspe: Added Script tag to access ionic-icons */}
+    <Script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></Script>
+    <Script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></Script>
+</nav>
+)
 }
 
 export default Navbar;
